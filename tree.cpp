2e855e3ed018c86505cecdd3644f611f53c21698
msl <912526205@qq.com>
2015-09-21 22:16:22 +0800

    calDepth of binary tree iteratively

--- tree.cpp
+++ tree.cpp
@@ -1,21 +1,49 @@
 #include<iostream>
 #include<sstream>
 #include<string>
+#include<queue>
 
 using namespace std;
 
 struct Node{
-    string val;
-    Node* left;
-    Node* right;
-    Node(string v,Node* l,Node* r):val(v),left(l),right(r){}
+	string val;
+	Node* left;
+	Node* right;
+	Node(string v,Node* l,Node* r):val(v),left(l),right(r){}
+	Node(string v):val(v),left(NULL),right(NULL){}
 };
 
-int  main(){
-    Node* n1= new Node("a",NULL,NULL);
-    Node n2=Node{"b",NULL,NULL};
-    cout<<n1->val<<","<<n2.val<<endl;
-	
+int calDepth(Node* r){
+	if(r==NULL)
+		return 0;
+	queue<Node*> q;
+	q.push(r);
+	q.push(NULL);
+	int h=0;
+	Node* t;
+	while(q.size()>0){
+		t = q.front();
+		q.pop();
+		if(t==NULL){
+			h++;
+			if(q.size()>0)
+				q.push(NULL);
+		} else {
+			if(t->left!=NULL)
+				q.push(t->left);
+			if(t->right!=NULL)
+				q.push(t->right);
+		}
+	}
+	return h;
+}
+
+int main(){
+	Node* n1 = new Node("a");
+	Node* n2 = new Node("b");
+	Node* n3 = new Node("c",n1,n2);
+	cout<<n1->val<<","<<n2->val<<endl;
+	cout<<calDepth(n3)<<","<<calDepth(n1)<<endl;	
 	stringstream s;
 	int i=1234;
 	string str;
@@ -23,5 +51,5 @@ int  main(){
 	s>>str;
 	cout<<str<<endl;
 
-    return 0;
+	return 0;
 }
